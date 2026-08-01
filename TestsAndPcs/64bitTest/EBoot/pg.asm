c909e1e8f23785736f1291d0495b38f37987da4d
ErickStudios <152753651+ErickStudios@users.noreply.github.com>
2026-07-31 20:50:35 -0600

    for holidays

--- TestsAndPcs/64bitTest/EBoot/pg.asm
+++ TestsAndPcs/64bitTest/EBoot/pg.asm
@@ -42,15 +42,15 @@ symbols:
     db          0
 
 align 20 print_ident: 
-    db 5,'P','R','I','N','T'
+    db 5,'p','r','i','n','t'
 align 20 input_ident: 
-    db 5,'I','N','P','U','T'
+    db 5,'i','n','p','u','t'
 align 20 goto_ident: 
-    db 4,'G','O','T','O'
+    db 4,'g','o','t','o'
 align 20 gosub_ident: 
-    db 5,'G','O','S','U','B'
+    db 5,'g','o','s','u','b'
 align 20 return_ident: 
-    db 6,'R','E','T','U','R','N'
+    db 6,'r','e','t','u','r','n'
 
     align 20
 vars:
@@ -65,18 +65,23 @@ basicsp: word 0
 reserve 128 logicalstack:
 bufferline: reserve 128
 testcode:
-    db  '0','3','0','+','0','2','4',0
+    db  'p','r','i','n','t','0','3','0','+','0','2','4',0
 tokens:     reserve (2*20)
+
+strtemp: db  0          ; count
+strtmpc: reserve 10     ; str content
+
 toki:       word 0
 
 bootstrap:
+    ; configure real stack
     li32    sp, logicalstack
-    mov     r0, 0
-    calc    r0
 
+    ; return basic sp
     li32    r0, retTOS
     bl      config_sp
 
+    ; parse the code
     li32    r0, testcode
     bl      parse
     
@@ -86,17 +91,17 @@ bootstrap:
     hlt
 
 config_sp:
-    li32    r7, basicsp
-    mwr16   r7, r0
-    jmp     lnk
+    li32    r7, basicsp ; stack ptr addr
+    mwr16   r7, r0      ; put stack
+    ret
 
 push_value_bsp:
-    li32    r7, basicsp
-    lv16    r7, r8
-    sub     r8, r8, 2
-    mwr16   r7, r8
-    mwr16   r8, r0
-    jmp     lnk
+    li32    r7, basicsp ; put stack
+    lv16    r7, r8      ; read pointer
+    sub     r8, r8, 2   ; sub 2
+    mwr16   r7, r8      ; write
+    mwr16   r8, r0      ; write stack
+    ret
 
 push_new_token:
     push    r0
@@ -124,48 +129,83 @@ push_new_token:
     pop     r0
     ret
 
+strcmp:
+    mov     r9, 0
+    push    lnk
+    pusha
+    mov     r2, [byte r0]   ; len1
+    mov     r3, [byte r1]   ; len2
+    cmp     r4, r2, r3
+    jz      strcmpokle
+    jmp     strcmpend
+strcmpokle:
+    inc     r0
+    inc     r1
+strcmploop:
+    cmp     r8, r2, 0
+    jz      scmpyesFinish
+    mov     r4, [byte r0]
+    mov     r5, [byte r1]
+    cmp     r4, r4, r5
+    jz      stcmpne
+    jmp     strcmpend
+stcmpne:
+    inc     r0
+    inc     r1
+    dec     r2
+    dec     r3
+    jmp     strcmploop
+scmpyesFinish:
+    mov     r9, 1
+strcmpend:
+    popa
+    pop     lnk
+    ret
+
 parse:
     push    lnk
 loopp:
     pop     lnk
     push    lnk
-    mov     r1, [byte r0]
-    cmp     r4, r1, 0
+
+    mov     r1, [byte r0]   ; character
+    cmp     r4, r1, 0       ; end of file
     jz      endParse
+
 tryParseSymbolStart:
-    add     r2, 0, (symbols/20)
-    mul     r2, r2, 20
-    mov     r7, TYUNKNOW
+    add     r2, 0, (symbols/20) ; little trap
+    mul     r2, r2, 20      ; multiply trap
+    mov     r7, TYUNKNOW    ; unknown symbol
 tryParseSymbol:
-    mov     r3, [byte r2]
-    cmp     r4, r1, r3
-    inc     r2
-    jz      isSymbol
-    cmp     r4, r3, 0
-    jz      iGiveUpItIsNotSymbol
+    mov     r3, [byte r2]   ; get symbol
+    cmp     r4, r1, r3      ; compare
+    inc     r2              
+    jz      isSymbol        ; is the symbol
+
+    cmp     r4, r3, 0       ; end?
+    jz      iGiveUpItIsNotSymbol ; yes
+
     jmp     tryParseSymbol
 
 isSymbol:
-    mov     r7, TYOPR
-    mov     r8, r3
-    jmp     yeahIFindIt
+    mov     r7, TYOPR       ; yes
+    mov     r8, r3          ; symbol
+    jmp     yeahIFindIt     ; i find
 iGiveUpItIsNotSymbol:
-    jmp     tryParseNumberMain
-
+    jmp     tryParseNumberMain ; try numbers?
 tryParseNumberMain:
-    cmp     r4, r1, '0'
-    jg      hmmINotSureNum
-    jz      isNumberPrefix
+    cmp     r4, r1, '0'         ; compare
+    jg      hmmINotSureNum      ; i not sure xD
+    jz      isNumberPrefix      ; yes is number
+
     jmp     endTryNum
 
 hmmINotSureNum:
-    cmp     r4, r1, '9'
-    jl      isNumberPrefix
-    jz      isNumberPrefix
-    jmp     endTryNum
+    cmp     r4, r1, '9'         ; verify bounds
+    jl      isNumberPrefix      ; is num
+    jz      isNumberPrefix      ; is num
+    jmp     iGiveUpItIsNotNumber ; no is not
 
-endTryNum:
-    jmp     iGiveUpItIsNotNumber
 isNumberPrefix:
     mov     r8, 0
     mov     r7, [byte r0]
@@ -181,45 +221,90 @@ isNumberPrefix:
     add     r8, r8, r7
     add     r0, r0, 2
     mov     r7, TYNUM
-    jmp     yeahIFindIt
+    jmp     yeahIFindIt         ; i find it
 iGiveUpItIsNotNumber:
-yeahIFindIt:
-    bl      push_new_token
+tryIdentifier:
+    cmp     r4, r1, 'a'
+    jz      tryIdentifierYes
+    jg      tryIdentifierYes
+    jmp     iGiveUpItIsNotIdentifier
+tryIdentifierYes:
+    li32    r8, strtemp
+    inc     r8
+    mov     r7, 0
+tidentLoop:
+    mov     r1, [byte r0]
+    cmp     r4, r1, 'a'
+    jl      endIdentifier
+    mwr8    r8, r1
     inc     r0
-    jmp     loopp
+    inc     r7
+    inc     r8
+    jmp     tidentLoop
+endIdentifier:
+    li32    r8, strtemp
+    mwr8    r8, r7              ; write len
+tryIdentifyWhatIdentIs:
+    li32    r6, identifiers
+loop50:
+    mov     r7, [byte r6]
+    mul     r7, r7, 20
+    push    r7
+    pusha
+    mov     r0, r8
+    mov     r1, r7
+    bl      strcmp
+    popa
+    pop     r7
+    cmp     r4, r9, 1
+    jz      isFinded
+    inc     r6
+    jmp     loop50
+isFinded:
+    mov     r8, r7
+    mov     r7, TYIDENT
+    hlt
+    jmp     yeahIFindIt
+iGiveUpItIsNotIdentifier:
+    hlt                         ; is nothing bye
+yeahIFindIt:
+    bl      push_new_token      ; push token
+    inc     r0                  ; inc char index
+    jmp     loopp               ; next entry
+
 endParse:
     pop     lnk
     li32    r0, tokens
     push    lnk
     mov     r7, TYEOF
-    mov     r8, 0
     bl      push_new_token
     pop     lnk
     ret
 
 runTok:
-    push    lnk
-
+    push    lnk             ; fix bug PPLS
 runTokLoop:
-    pop     lnk
-    push    lnk
+    pop     lnk             ; xd
+    push    lnk             ; a
 
-    bl      runPeek
+    bl      runPeek         ; check token
     cmp     r3, r1, TYEOF   ; verify file end
     jz      end
 
-    cmp     r3, r1, TYNUM   ; number
-    jz      tryPlayNum
+    cmp     r3, r1, TYNUM   ; check if number
+    jz      tryPlayNum      ; yes
 
     jmp     ignoreItsNotNum ; is not number
 
 tryPlayNum:
     bl      runConsume      ; consume it
-    mov     r7, r2
+    mov     r7, r2          ; save number to fixed HEHE
     bl      runPeek         ; peek
-    cmp     r1, r1, TYOPR
+    
+    cmp     r1, r1, TYOPR   ; verify if operator
     jz      isOpr
-    jmp     isNormalNum
+    
+    jmp     isNormalNum     ; no, continue
 
 ; ============ OPERATIONS TABLE ============
 oprTable:
@@ -240,23 +325,32 @@ runSubstract:   sub r7, r7, r8
 runDivition:    div r7, r7, r8 
                 ret
 isOpr:
-    bl      runConsume
+    bl      runConsume      ; check operator
     mov     r9, r2
-    bl      runConsume
+    bl      runConsume      ; check second number
     mov     r8, r2
-    li32    r4, oprTable
-    sub     r9, r9, '*'
-    mul     r9, r9, 2
-    add     r4, r4, r9
-    mov     r4, [word r4]
-    bl      r4
+
+    push    r7
+    cmp     r7, r1, TYNUM   ; check if number
+    pop     r7
+    jz      okIsNumberOp2
+    jmp     syntax_error    ; no, its not, frozen CPU
+
+okIsNumberOp2:
+    li32    r4, oprTable    ; operation table
+    sub     r9, r9, '*'     ; trap for smaller code
+    mul     r9, r9, 2       ; its word
+    add     r4, r4, r9      ; add
+
+    mov     r4, [word r4]   ; get label
+    bl      r4              ; jump and link
 isNormalNum:
 ignoreItsNotNum:
-    jmp     runTokLoop
-end:
-    pop     lnk
+    jmp     runTokLoop      ; jump
+    
+end:pop     lnk             ; recovery link reg
     ret
-
+syntax_error: hlt           ; error of syntax
 runPeek:
     mov     r1, [byte r0+0] ; get type
     mov     r2, [byte r0+1] ; get value
